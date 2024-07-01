cdf44bcd13065faba004241579425cf631530eac
TshepoMelesi <twmelesi@gmail.com>
2024-07-01 02:37:01 +0200

    added just a home route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app  = express()
 const PORT = process.env.PORT || 3000
 
 
+app.use("/", (req, res) => {
+        res.send({"message" : "hello there"})
+})
+
 app.listen(PORT, () => {
         console.log("connected at " + PORT)
 })
